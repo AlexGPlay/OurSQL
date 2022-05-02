b2fa92c98103a53069ba2758833b4794ed6df8a5
AlexGPlay <alexgp1234@gmail.com>
2022-05-02 19:12:01 +0200

    Add tests for add fn

--- tests/leaf_spec.rs
+++ tests/leaf_spec.rs
@@ -16,4 +16,33 @@ mod tests {
         assert_eq!(format!("{:?}", btree_leaf), "Leaf { degree: 5, keys: [Node { key: 1 }], children: [] }");
     }
 
+    // Tests if the add method returns 0 while the leaf has space.
+    #[test]
+    fn test_leaf_add_enough_space(){
+        let mut btree_leaf: BTreeLeaf<u8> = BTreeLeaf::new(5);
+        
+        for n in 0..4 {
+            let result = btree_leaf.add(n);
+            assert_eq!(result, 0);
+        }
+
+        assert_eq!(format!("{:?}", btree_leaf), "Leaf { degree: 5, keys: [Node { key: 0 }, Node { key: 1 }, Node { key: 2 }, Node { key: 3 }], children: [] }");
+    }
+
+        // Tests if the add method returns 1 for a full leaf.
+        #[test]
+        fn test_leaf_add_not_enough_space(){
+            let mut btree_leaf: BTreeLeaf<u8> = BTreeLeaf::new(5);
+            
+            for n in 0..4 {
+                let result = btree_leaf.add(n);
+                assert_eq!(result, 0);
+            }
+
+            let result = btree_leaf.add(4);
+            assert_eq!(result, 1);
+    
+            assert_eq!(format!("{:?}", btree_leaf), "Leaf { degree: 5, keys: [Node { key: 0 }, Node { key: 1 }, Node { key: 2 }, Node { key: 3 }, Node { key: 4 }], children: [] }");
+        }
+
 }
